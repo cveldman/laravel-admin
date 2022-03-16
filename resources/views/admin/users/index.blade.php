2499b30ca29acc771ff0e07fd7fb43d93fd3ae04
Corné Veldman <cveldman@hotmail.com>
2022-03-16 11:12:06 +0100

    add user button

--- resources/views/admin/users/index.blade.php
+++ resources/views/admin/users/index.blade.php
@@ -2,35 +2,45 @@
 
 @section('content')
 
-        <h1 class="text-2xl font-semibold text-gray-900">Users</h1>
+    <div class="sm:flex sm:items-center">
+        <div class="sm:flex-auto">
+            <h1 class="text-xl font-semibold text-gray-900">Users</h1>
+            <p class="mt-2 text-sm text-gray-700">A list of all the users in your account including their name, title, email and role.</p>
+        </div>
+        <div class="mt-4 sm:mt-0 sm:ml-16 sm:flex-none">
+            <a href="{{ route('admin.users.create') }}" type="button" class="inline-flex items-center justify-center rounded-md border border-transparent bg-indigo-600 px-4 py-2 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 sm:w-auto">
+                Add user
+            </a>
+        </div>
+    </div>
 
-        <x-data-table :datatable="$users">
-            <x-slot name="columns">
-                <x-dcolumn order="name">Name</x-dcolumn>
-                <x-dcolumn order="email">Email</x-dcolumn>
-                <x-dcolumn></x-dcolumn>
-            </x-slot>
-            @foreach($users as $user)
-                <x-row>
-                    <td class="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900">
-                        <a href="{{ route('admin.users.show', $user) }}">{{ $user->name }}</a>
-                    </td>
+    <x-data-table :datatable="$users">
+        <x-slot name="columns">
+            <x-dcolumn order="name">Name</x-dcolumn>
+            <x-dcolumn order="email">Email</x-dcolumn>
+            <x-dcolumn></x-dcolumn>
+        </x-slot>
+        @foreach($users as $user)
+            <x-row>
+                <td class="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900">
+                    <a href="{{ route('admin.users.show', $user) }}">{{ $user->name }}</a>
+                </td>
 
-                    <x-cell>{{ $user->email }}</x-cell>
+                <x-cell>{{ $user->email }}</x-cell>
 
-                    <td class="px-6 py-4 whitespace-nowrap text-right text-sm font-medium">
-                        @can('update', $user)
-                            <a href="{{ route('admin.users.edit', $user) }}"
-                               class="text-indigo-600 hover:text-indigo-900">Edit</a>
-                        @endcan
+                <td class="px-6 py-4 whitespace-nowrap text-right text-sm font-medium">
+                    @can('update', $user)
+                        <a href="{{ route('admin.users.edit', $user) }}"
+                           class="text-indigo-600 hover:text-indigo-900">Edit</a>
+                    @endcan
 
-                        @can('delete', $user)
-                            <a href="{{ route('admin.users.edit', $user) }}"
-                               class="text-indigo-600 hover:text-indigo-900">Edit</a>
-                        @endcan
-                    </td>
-                </x-row>
-            @endforeach
-        </x-data-table>
+                    @can('delete', $user)
+                        <a href="{{ route('admin.users.edit', $user) }}"
+                           class="text-indigo-600 hover:text-indigo-900">Edit</a>
+                    @endcan
+                </td>
+            </x-row>
+        @endforeach
+    </x-data-table>
 
 @endsection
--- src/Http/Controllers/App/LoginController.php
+++ src/Http/Controllers/App/LoginController.php
@@ -3,6 +3,7 @@
 namespace Veldman\Admin\Http\Controllers\App;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Veldman\Admin\Http\Controllers\Controller;
 
 class LoginController extends Controller
@@ -19,7 +20,11 @@ class LoginController extends Controller
 
     public function login(Request $request)
     {
-        //
+        if(Auth::guard()->attempt($request->only('email', 'password'), $request->filled('remember'))) {
+            return redirect('/admin');
+        }
+
+        return redirect('/admin/login');
     }
 
     public function logout()
