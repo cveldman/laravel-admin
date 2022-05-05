4d638da0661312148fe56b1026ffa56839973f5a
Corné Veldman <cveldman@hotmail.com>
2022-05-05 22:24:39 +0200

    header nieuw design users

--- resources/views/admin/users/index.blade.php
+++ resources/views/admin/users/index.blade.php
@@ -2,15 +2,13 @@
 
 @section('content')
 
-    <!-- <div class="px-4 sm:px-6 lg:px-8"> -->
     <div class="sm:flex sm:items-center">
         <div class="sm:flex-auto">
-            <h1 class="text-xl font-semibold text-gray-900">{{ __('admin::users.title') }}</h1>
+            <h1 class="text-3xl font-semibold text-gray-900">{{ __('admin::users.title') }}</h1>
             <p class="mt-2 text-sm text-gray-700">{{ __('admin::users.description') }}</p>
         </div>
         <div class="mt-4 sm:mt-0 sm:ml-16 sm:flex-none">
-            <a href="{{ route('admin.users.create') }}" type="button"
-               class="inline-flex items-center justify-center rounded-md border border-transparent bg-indigo-600 px-4 py-2 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 sm:w-auto">
+            <a href="{{ route('admin.users.create') }}" type="button" class="inline-flex items-center justify-center rounded-md border border-transparent bg-indigo-600 px-4 py-2 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 sm:w-auto">
                 {{ __('Add :resource', ['resource' => __('admin::users.resource')]) }}
             </a>
         </div>
