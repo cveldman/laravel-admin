1f74e6b5e99d060202832ffef3a809fad67f9512
Corné Veldman <cveldman@hotmail.com>
2022-05-12 14:54:47 +0200

    id app out of layout

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -25,7 +25,7 @@
             @include('admin::layout.sidebar')
             <div class="md:pl-64 flex flex-col flex-1">
                 @include('admin::layout.header')
-                <main id="app">
+                <main>
                     <div class="py-6">
                         <!-- <div class="max-w-7xl mx-auto px-4 sm:px-6 md:px-8">
                             <h1 class="text-2xl font-semibold text-gray-900">Dashboard</h1>
