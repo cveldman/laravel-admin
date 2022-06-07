166a2213bbe072c3e73aabdf5c7786984b8ffdf3
Corné Veldman <cveldman@hotmail.com>
2022-06-07 10:11:31 +0200

    Select werkend gemaakt

--- src/View/Components/Select.php
+++ src/View/Components/Select.php
@@ -9,11 +9,13 @@ class Select extends Component
     public $type;
     public $name;
     public $options;
+    public $value;
 
-    public function __construct($type = 'text', $name, $options)
+    public function __construct($type = 'text', $name, $options, $value = null)
     {
         $this->type = $type;
         $this->name = $name;
+        $this->value = old($name, $value);
         $this->options = $options;
     }
 
